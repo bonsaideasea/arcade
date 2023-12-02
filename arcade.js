ce68e3fdedacff30061cab60db83750db0194481
bonsaideasea <bonsaideasea@gmail.com>
2023-12-02 15:11:24 -0800

    before the glitch effects

---
+++ arcade.js
@@ -0,0 +1,11 @@
+const hideAP = document.querySelector('body');
+
+hideAP.onclick = () => {
+  hideAP.style.display = 'none';
+  //let blankScreen = hideAP.style.display;
+  //hideAP.style.backgroundColor = "black";
+}
+
+hideAP.onkeyup = () => {
+   hideAP.style.display = 'none';
+}
